3ea4b7e6cdebe7f78fcdca2958dd066071c4287a
MASKOUR ELHOUCINE <elhoucine.maskour7@gmail.com>
2023-06-12 17:39:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This README file provides instructions for training a YOLOv7 model for logo detection using the provided code and dataset.
+
+# Getting Started
---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+# Clone the Darknet repository
+# !git clone https://github.com/AlexeyAB/darknet.git
+
+# Change to darknet directory
+os.chdir('darknet')
+
+# Download pre-trained YOLO weights
+# !wget https://pjreddie.com/media/files/darknet53.conv.74
